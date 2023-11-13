0cc0006f7011815dcc10b92e84d3b5387f96d448
Vaengir <vaengir@gmx.de>
2023-11-13 14:48:41 +0100

    style: Change to iterators

--- RostBuch/minigrep/src/lib.rs
+++ RostBuch/minigrep/src/lib.rs
@@ -7,17 +7,20 @@ pub struct Config {
 }
 
 impl Config {
-  pub fn build(args: &[String]) -> Result<Config, &'static str> {
-    let mut ignore_case: bool = false;
-    if args.len() < 3 || args.len() > 4 {
-      return Err("Not the right amount of arguments, expected 2 or 3");
-    } else if args.len() == 3 {
-      ignore_case = env::var("IGNORE_CASE").is_ok();
-    } else if args.len() == 4 {
-      ignore_case = args[3].parse::<bool>().unwrap().to_owned();
-    }
-    let query = args[1].to_owned();
-    let file_path = args[2].to_owned();
+  pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+    args.next();
+
+    let query = match args.next() {
+      Some(arg) => arg,
+      None => return Err("Didn't get a query string"),
+    };
+
+    let file_path = match args.next() {
+      Some(arg) => arg,
+      None => return Err("Didn't get a file path"),
+    };
+
+    let ignore_case = env::var("IGNORE_CASE").is_ok();
 
     Ok(Config {
       query,
@@ -44,26 +47,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-  let mut results = Vec::new();
-
-  for line in contents.lines() {
-    if line.contains(query) {
-      results.push(line);
-    }
-  }
-  results
+  contents
+    .lines()
+    .filter(|line| line.contains(query))
+    .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-  let query = query.to_lowercase();
-  let mut results = Vec::new();
-
-  for line in contents.lines() {
-    if line.to_lowercase().contains(&query) {
-      results.push(line);
-    }
-  }
-  results
+  contents
+    .lines()
+    .filter(|line| line.contains(&query.to_lowercase()))
+    .collect()
 }
 
 #[cfg(test)]
