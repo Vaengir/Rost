d04d1992ecf85db3933a0eb5032e3f0cf5c1b76e
Vaengir <vaengir@gmx.de>
2023-11-19 20:42:42 +0100

    feat: Chapter references

--- RostBuch/reference_cycle/src/main.rs
+++ RostBuch/reference_cycle/src/main.rs
@@ -1,7 +1,12 @@
 use crate::List::{Cons, Nil};
 
 use std::cell::RefCell;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
+
+fn main() {
+  bad_cycle();
+  good_cycle();
+}
 
 #[derive(Debug)]
 enum List {
@@ -18,7 +23,7 @@ impl List {
   }
 }
 
-fn main() {
+fn bad_cycle() {
   let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
   println!("a initial rc count = {}", Rc::strong_count(&a));
   println!("a next item = {:?}", a.tail());
@@ -37,4 +42,54 @@ fn main() {
 
   // Uncomment the next line to see that we have a cycle; it will overflow the stack
   // println!("a next item = {:?}", a.tail());
+}
+
+#[derive(Debug)]
+struct Node {
+  value: i32,
+  parent: RefCell<Weak<Node>>,
+  children: RefCell<Vec<Rc<Node>>>,
+}
+
+fn good_cycle() {
+  let leaf = Rc::new(Node {
+    value: 3,
+    parent: RefCell::new(Weak::new()),
+    children: RefCell::new(vec![]),
+  });
+
+  println!(
+    "leaf strong = {}, weak = {}",
+    Rc::strong_count(&leaf),
+    Rc::weak_count(&leaf),
+  );
+
+  {
+    let branch = Rc::new(Node {
+      value: 5,
+      parent: RefCell::new(Weak::new()),
+      children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    println!(
+      "branch strong = {}, weak = {}",
+      Rc::strong_count(&branch),
+      Rc::weak_count(&branch),
+    );
+
+    println!(
+      "leaf strong = {}, weak = {}",
+      Rc::strong_count(&leaf),
+      Rc::weak_count(&leaf),
+    );
+  }
+
+  println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+  println!(
+    "leaf strong = {}, weak = {}",
+    Rc::strong_count(&leaf),
+    Rc::weak_count(&leaf),
+  );
 }
