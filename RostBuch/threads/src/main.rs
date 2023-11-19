5c9ff91523e9f6317cbb32f21f93459528796b52
Vaengir <vaengir@gmx.de>
2023-11-19 20:42:59 +0100

    WIP: feat: Chapter concurrency

---
+++ RostBuch/threads/src/main.rs
@@ -0,0 +1,32 @@
+use std::{thread, time::Duration};
+
+fn main() {
+  thread_01();
+  move_thread();
+}
+
+fn thread_01() {
+  let handle = thread::spawn(|| {
+    for i in 1..10 {
+      println!("hi number {} from the spawned thread!", i);
+      thread::sleep(Duration::from_millis(1));
+    }
+  });
+
+  handle.join().unwrap();
+
+  for i in 1..5 {
+    println!("hi number {} from the main thread!", i);
+    thread::sleep(Duration::from_millis(1));
+  }
+}
+
+fn move_thread() {
+  let v = vec![1, 2, 3];
+
+  let handle = thread::spawn(move || {
+    println!("Here's the vector: {:?}", v);
+  });
+
+  handle.join().unwrap();
+}
